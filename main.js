83d5357d78a892f36643882b49bfedf5ed0164f2
Joey Quandt <joeyquandt@outlook.com>
2022-05-21 14:39:51 +0200

    navigation bar

---
+++ main.js
@@ -0,0 +1,25 @@
+
+/*Hamburger Section*/
+const nav = document.querySelector(".nav-links");
+const burger = document.querySelector(".burger");
+const links = nav.querySelectorAll("a");
+
+burger.addEventListener("click", () => {
+  nav.classList.toggle("nav-open");
+  burger.classList.toggle("toggle");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.classList.toggle("nav-open");
+    burger.classList.toggle("toggle");
+  });
+});
+
+//Navigation Bar color change//
+window.addEventListener("scroll", function () {
+    let header = document.querySelector("header");
+    let windowPosition = window.scrollY > 0;
+  
+    header.classList.toggle("scrolling-active", windowPosition);
+  });
